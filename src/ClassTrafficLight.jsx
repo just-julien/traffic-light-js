0421ba5d19b5f35ed10b38f47e67c630851d471c
justjulien <i_sauce_u@steelrock.localdomain>
2025-11-21 13:17:35 -0600

    changes to class and functional based traffic lights to complete assignment

--- src/ClassTrafficLight.jsx
+++ src/ClassTrafficLight.jsx
@@ -1,17 +1,33 @@
 import { Component } from "react";
 
 export class ClassTrafficLight extends Component {
+
+  state = {
+    currentState: 0,
+  };
+
+  states = ["red", "yellow", "green"];
+
+  nextState = () => {
+    this.setState((prevState) => ({
+      currentState: (prevState.currentState + 1) % this.states.length,
+    }));
+  };
+
   render() {
+    const { currentState } = this.state;
+    const currentColor = this.states[currentState];
+
     return (
       <div className="traffic-light-box">
         <h2>Class Traffic Light</h2>
         <div className="traffic-light">
           {/* Background color can be black | yellow | red | green */}
-          <div className="circle red"></div>
-          <div className="circle black"></div>
-          <div className="circle black"></div>
+          <div className={`circle ${currentColor === "red" ? "red" : "black"}`}></div>
+          <div className={`circle ${currentColor === "yellow" ? "yellow" : "black"}`}></div>
+          <div className={`circle ${currentColor === "green" ? "green" : "black"}`}></div>
         </div>
-        <button className="next-state-button">Next State</button>
+        <button className="next-state-button" onClick={this.nextState}>Next State</button>
       </div>
     );
   }
--- src/FunctionalTrafficLight.jsx
+++ src/FunctionalTrafficLight.jsx
@@ -1,14 +1,24 @@
+import { useState } from "react";
+
 export const FunctionalTrafficLight = () => {
+
+  const states = ["red", "yellow", "green"];
+  const [currentState, setCurrentState] = useState(0);
+
+  const nextState = () => {
+    setCurrentState((prev) => (prev + 1) % states.length);
+  };
+
   return (
     <div className="traffic-light-box">
       <h2>Functional Traffic Light</h2>
       <div className="traffic-light">
         {/* Background color can be black | yellow | red | green */}
-        <div className="circle black"></div>
-        <div className="circle yellow"></div>
-        <div className="circle green"></div>
+        <div className={`circle ${states[currentState] === "red" ? "red" : "black"}`}></div>
+        <div className={`circle ${states[currentState] === "yellow" ? "yellow" : "black"}`}></div>
+        <div className={`circle ${states[currentState] === "green" ? "green" : "black"}`}></div>
       </div>
-      <button className="next-state-button">Next State</button>
+      <button className="next-state-button" onClick={nextState}>Next State</button>
     </div>
   );
 };
